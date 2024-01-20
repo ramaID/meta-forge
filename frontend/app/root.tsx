62d199af370b1dda17af6ae565d3f6413ada1e74
rama <rama@javan.co.id>
2024-01-20 15:07:38 +0700

    add: maximum-scale

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -20,7 +20,10 @@ export default function App() {
     <html lang="en" className="h-full bg-gray-50">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <Meta />
         <Links />
       </head>
